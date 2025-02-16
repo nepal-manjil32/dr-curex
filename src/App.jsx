4786c09bcab04e0d2c508054b5fc103f83a9ab71
jaayash <116748731+jaayash@users.noreply.github.com>
2025-02-16 19:08:38 +0530

    added landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
-import Login from './components/Login/Login';
-import SignUp from './components/SignUp/SignUp';
+import React from "react";
+import "./App.css";
+import Login from "./components/Login/Login";
+import SignUp from "./components/SignUp/SignUp";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
     <>
       <div className="landing-page">
-        <SignUp />
+        <div className="landing-left">
+          <img src="src/assets/dr.jpg" alt="doctor-img" />
+        </div>
+        <div className="landing-right">
+          <Navbar />
+          <h1>FROM SYMPTOMS TO SOLUTIONS</h1>
+          <h2>AI-DRIVEN HEALTHCARE FOR YOU</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin <br />hendrerit nunc pharetra elit vehicula blandit. Curabitu
+          </p>
+
+          <div className="button-container">
+            <div className="getting-started">
+              <a href="">Gettng Started</a>
+            </div>
+            <div className="read-more">
+              <a href="">Read More</a>
+            </div>
+          </div>
+          <h3>SPECIAL DISCOUNT</h3>
+          <h3>UPTO 40% FOR FIRST TIME USERS</h3>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default App;
